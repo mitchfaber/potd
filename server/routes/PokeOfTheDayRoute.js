06b9e31ca338c20b880c361e5744df5983e24cab
Mitch Faber <faberskits@gmail.com>
2022-06-14 12:40:22 -0300

    mysql interaction, changed route

--- server/routes/PokeOfTheDayRoute.js
+++ server/routes/PokeOfTheDayRoute.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
-console.log(process.env.password);
 const express = require("express");
 const router = express.Router();
+const scheduler = require("node-schedule");
+let todaysMon;
 
 let mysql = require("mysql");
 
+scheduler.scheduleJob("0 0 * * *", () => {});
 let con = mysql.createConnection({
 	host: process.env.url,
 	user: process.env.username,
@@ -14,6 +16,17 @@ let con = mysql.createConnection({
 
 con.connect(function (err) {
 	if (err) throw err;
+	let today = new Date();
+	let dd = String(today.getDate()).padStart(2, "0");
+	let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+	let yyyy = today.getFullYear();
+	today = yyyy + "-" + mm + "-" + dd;
+	console.log(today);
+	let sql = "SELECT * FROM PokemonOfTheDay WHERE Date = ?";
+	con.query(sql, [today], (err, result) => {
+		if (err) throw err;
+		todaysMon = result;
+	});
 	console.log("Connected!");
 });
 
