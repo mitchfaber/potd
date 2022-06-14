c8a39c176747ff5c976e72c3db8d6fe3f05e2e56
Mitch Faber <faberskits@gmail.com>
2022-06-14 07:40:55 -0300

    working on mysql interaction

--- server/routes/PokeOfTheDayRoute.js
+++ server/routes/PokeOfTheDayRoute.js
@@ -1,5 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+
+let mysql = require("mysql");
+
+let con = mysql.createConnection({
+	host: "mitchfaber.ca",
+	user: "mitchfab_faberm",
+	password: "9kHhJDR5",
+	database: "mitchfab_pokemon",
+});
+
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
 
 router.get("/", (req, res) => {
 	res.send("Pokemon of the day");
--- server/server.js
+++ server/server.js
@@ -11,20 +11,6 @@ app.use("/generation", genRouter);
 const potdRouter = require("./routes/PokeOfTheDayRoute");
 app.use("/", potdRouter);
 
-let mysql = require("mysql");
-
-let con = mysql.createConnection({
-	host: "mitchfaber.ca",
-	user: "mitchfab_faberm",
-	password: "9kHhJDR5",
-	database: "mitchfab_pokemon",
-});
-
-con.connect(function (err) {
-	if (err) throw err;
-	console.log("Connected!");
-});
-
 app.listen(8080, () => {
 	console.log("Server Started");
 });
