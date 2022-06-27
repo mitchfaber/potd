256598a1e0f89fb0df024d559ae236ae061e492b
Mitch Faber <faberskits@gmail.com>
2022-06-27 12:56:22 -0300

    - adding color types to styles. - started work on frontend. Generating sprite,type,name

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import Pokemon from "./Pokemon";
+import PokemonList from "./PokemonList";
+
 function App() {
-	return <button className="btn btn-primary">Hello World</button>;
+	const [loading, setLoading] = useState(true);
+	const [spotlightDate, setSpotlightDate] = useState();
+
+	let today = new Date();
+	let dd = String(today.getDate()).padStart(2, "0");
+	let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+	let yyyy = today.getFullYear();
+	today = yyyy + "-" + mm + "-" + dd;
+
+	useEffect(() => {
+		setSpotlightDate(today);
+		setLoading(false);
+	}, []);
+
+	function setSpotDate(newDate) {
+		console.log(newDate);
+		setSpotlightDate(newDate);
+	}
+
+	if (loading) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			<div className="container">
+				<Pokemon spotlightDate={spotlightDate} today={today}></Pokemon>
+				<PokemonList setSpotDate={setSpotDate} today={today}></PokemonList>
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/PokemonList.js
+++ client/src/PokemonList.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function PokemonList() {
-	return <div>PokemonList</div>;
+export default function PokemonList({ setSpotDate, today }) {
+	const [curPokemon, setCurPokemon] = useState();
+	const [loading, setLoading] = useState(false);
+
+	// useEffect(() => {
+	// 	setSpotDate(today);
+	// }, []);
+	return (
+		<div>
+			<input
+				type="date"
+				min="2022-06-13"
+				max={today}
+				onInput={(e) => {
+					setSpotDate(e.target.value);
+				}}></input>
+		</div>
+	);
 }
