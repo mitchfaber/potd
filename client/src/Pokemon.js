9bfefd8ca396d62dfc42d10223f7fba27ef47421
Mitch Faber <faberskits@gmail.com>
2022-06-28 21:28:54 -0300

    - Renamed to DatePicker, more appropriate. - some styling added, working on layout. - reformatted some classes in SCSS.

--- client/src/Pokemon.js
+++ client/src/Pokemon.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Pokemon({ spotlightDate, today }) {
 	const [pokemon, setPokemon] = useState();
@@ -6,6 +7,7 @@ export default function Pokemon({ spotlightDate, today }) {
 
 	useEffect(() => {
 		console.log(today);
+		setLoading(true);
 		if (spotlightDate === today) {
 			fetch(`http://localhost:8080/spotlight/`)
 				.then((res) => res.json())
@@ -33,12 +35,26 @@ export default function Pokemon({ spotlightDate, today }) {
 		return <div>Loading...</div>;
 	} else {
 		return (
-			<div>
-				{capitalize(pokemon.name)}
-				{pokemon.types.map((element) => {
-					return <div className={element.type.name}>{capitalize(element.type.name)}</div>;
-				})}
-				<img src={`${pokemon.sprites.front_default}`} alt={pokemon.name}></img>
+			<div className="container">
+				<div className="row mt-3 align-items-center justify-content-center">
+					<div className="alert alert-dark justify-content-center">
+						<h1>{capitalize(pokemon.name)}</h1>
+						<hr />
+						<div className="card-text">
+							{pokemon.types.map((element) => {
+								return (
+									<span key={uuidv4()} className={element.type.name}>
+										{capitalize(element.type.name)}
+									</span>
+								);
+							})}
+						</div>
+						<img
+							src={`${pokemon.sprites.other["official-artwork"].front_default}`}
+							className="img-fluid"
+							alt={pokemon.name}></img>
+					</div>
+				</div>
 			</div>
 		);
 	}
