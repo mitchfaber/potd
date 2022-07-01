08a37be621278ba55e4b94a831f5247562374e04
Mitch Faber <faberskits@gmail.com>
2022-07-01 17:24:18 -0300

    - Added scheduled task. - Moved som sql functions to a module

--- server/routes/PokeOfTheDayRoute.js
+++ server/routes/PokeOfTheDayRoute.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
-const mysql = require("mysql");
-// const scheduler = require("node-schedule");
 const NodeCache = require("node-cache");
 const myCache = new NodeCache();
+const sqlUtil = require("./../lib/Sql");
 
 // --------- ROUTES
 router.get("/spotlight", (req, res) => {
@@ -15,25 +14,9 @@ router.get("/spotlight", (req, res) => {
 	let yyyy = today.getFullYear();
 	today = yyyy + "-" + mm + "-" + dd;
 
-	getPotdSql(today).then((result) => {
+	sqlUtil.getPotdSql(today).then((result) => {
 		if (result.length === 0) {
-			console.log("no sql record found");
-			// No record in SQL for today, get random num and write to sql.
-			randomNum().then((result) => {
-				getCache(today).then((cacheRes) => {
-					if (cacheRes !== undefined) {
-						console.log("cache");
-						setPotdSql(cacheRes.name, cacheRes.id, today);
-						res.send(cacheRes);
-					} else {
-						console.log("api");
-						getFromAPI(result, today).then((e) => {
-							setPotdSql(e.name, e.id, today);
-							res.send(e);
-						});
-					}
-				});
-			});
+			res.send({ message: "No Pokemon found..." });
 		} else {
 			getCache(today).then((cacheRes) => {
 				if (cacheRes !== undefined) {
@@ -50,43 +33,15 @@ router.get("/spotlight", (req, res) => {
 	});
 });
 
-router.get("spotlight/dateLimits", (req, res) => {
-	let con = mysql.createConnection({
-		host: process.env.url,
-		user: process.env.username,
-		password: process.env.password,
-		database: process.env.dbname,
-	});
-	return new Promise((resolve) => {
-		con.connect(function (err) {
-			if (err) {
-				handleDisconnect();
-			}
-			let sql = "SELECT MAX(Date),Min(DATE) FROM PokemonOfTheDay";
-			con.query(sql, [today], (err, result) => {
-				if (err) {
-					handleDisconnect();
-				}
-				resolve(result);
-			});
-		});
-
-		con.on("error", function (err) {
-			console.log("db error", err);
-
-			if (err.code === "PROTOCOL_CONNECTION_LOST") {
-				// Connection to the MySQL server is usually lost due to either server restart
-				handleDisconnect();
-			} else {
-				throw err;
-			}
-		});
+router.get("/dateLimits", (req, res) => {
+	sqlUtil.getMinMaxDates().then((result) => {
+		res.send(result);
 	});
 });
 
 router.get("/spotlight/:date", (req, res) => {
 	let prevSpotlightDate = req.params.date;
-	getPotdSql(prevSpotlightDate).then((result) => {
+	sqlUtil.getPotdSql(prevSpotlightDate).then((result) => {
 		if (result.length === 0) {
 			console.log("no sql record found");
 			res.send({ message: "No pokemon found..." });
@@ -107,10 +62,6 @@ router.get("/spotlight/:date", (req, res) => {
 });
 
 // functions
-async function randomNum() {
-	return Math.floor(Math.random() * 898) + 1;
-}
-
 function getFromAPI(pokeID, today) {
 	return new Promise((resolve) => {
 		axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID}`).then((res) => {
@@ -131,98 +82,6 @@ function writeCache(data, today) {
 async function getCache(today) {
 	return new Promise((resolve) => {
 		resolve(myCache.get(today));
-	});
-}
-
-async function getPotdSql(today) {
-	let con = mysql.createConnection({
-		host: process.env.url,
-		user: process.env.username,
-		password: process.env.password,
-		database: process.env.dbname,
-	});
-	return new Promise((resolve) => {
-		con.connect(function (err) {
-			if (err) {
-				handleDisconnect();
-			}
-			let sql = "SELECT * FROM PokemonOfTheDay WHERE Date = ?";
-			con.query(sql, [today], (err, result) => {
-				if (err) {
-					handleDisconnect();
-				}
-				resolve(result);
-			});
-		});
-
-		con.on("error", function (err) {
-			console.log("db error", err);
-
-			if (err.code === "PROTOCOL_CONNECTION_LOST") {
-				// Connection to the MySQL server is usually lost due to either server restart
-				handleDisconnect();
-			} else {
-				throw err;
-			}
-		});
-	});
-}
-
-async function setPotdSql(pokeName, pokeID, today) {
-	let con = mysql.createConnection({
-		host: process.env.url,
-		user: process.env.username,
-		password: process.env.password,
-		database: process.env.dbname,
-	});
-
-	await con.connect(function (err) {
-		if (err) {
-			handleDisconnect();
-		}
-		let sql = "INSERT INTO PokemonOfTheDay (PokemonName, PokedexNum, Date) VALUES (?,?,?)";
-		con.query(sql, [pokeName, pokeID, today], (err, result) => {
-			if (err) {
-				handleDisconnect();
-			}
-		});
-	});
-
-	con.on("error", function (err) {
-		console.log("db error", err);
-		if (err.code === "PROTOCOL_CONNECTION_LOST") {
-			// Connection to the MySQL server is usually lost due to either server restart
-			handleDisconnect();
-		} else {
-			throw err;
-		}
-	});
-}
-
-function handleDisconnect() {
-	con = mysql.createConnection({
-		host: process.env.url,
-		user: process.env.username,
-		password: process.env.password,
-		database: process.env.dbname,
-	});
-
-	con.connect(function (err) {
-		// The server is either down or restarting (takes a while sometimes).
-		if (err) {
-			console.log("error when connecting to db:", err);
-			// Introduce a delay before attempting to reconnect, to avoid a hot loop
-			setTimeout(handleDisconnect, 2000);
-		}
-	});
-	con.on("error", function (err) {
-		console.log("db error", err);
-		if (err.code === "PROTOCOL_CONNECTION_LOST") {
-			// Connection to the sql server is usually lost due to either server restart
-			handleDisconnect();
-		} else {
-			throw err;
-		}
 	});
 }
 
