8bcb7c2577fec9b9d59984af2f347c7691c817e9
Mitch Faber <faberskits@gmail.com>
2022-07-03 10:13:13 -0300

    MinMaxDates working

--- client/src/DatePicker.js
+++ client/src/DatePicker.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 export default function DatePicker({ setSpotDate }) {
-	const [maxDate, setMaxDate] = useState();
-	const [minDate, setMinDate] = useState();
+	const [maxDate, setMaxDate] = useState("");
+	const [minDate, setMinDate] = useState("");
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
@@ -10,14 +10,27 @@ export default function DatePicker({ setSpotDate }) {
 		fetch(`http://localhost:8080/dateLimits/`)
 			.then((res) => res.json())
 			.then((result) => {
-				setMaxDate(result[0].max);
-				setMinDate(result[0].min);
+				console.log(result[0]);
+				setMaxDate(formatDate(result[0].max));
+				setMinDate(formatDate(result[0].min));
 			});
 	}, []);
 
 	useEffect(() => {
+		console.log(minDate);
+		console.log(maxDate);
 		setLoading(false);
 	}, [minDate, maxDate]);
+
+	function formatDate(dateToFormat) {
+		let formattedDate = new Date(Date.parse(dateToFormat));
+		formattedDate = new Date(formattedDate);
+		let dd = String(formattedDate.getUTCDate()).padStart(2, "0");
+		let mm = String(formattedDate.getUTCMonth() + 1).padStart(2, "0"); //January is 0!
+		let yyyy = formattedDate.getFullYear();
+		formattedDate = yyyy + "-" + mm + "-" + dd;
+		return formattedDate;
+	}
 
 	if (loading) {
 		return <div>Loading...</div>;
